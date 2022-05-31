b805daf03a60c98808b26206a813ad32051d17c3
phong <phong@.com>
2022-05-31 11:06:59 +0700

    up file

--- Bai7_2Sau.java
+++ Bai7_2Sau.java
@@ -9,10 +9,10 @@ public class Bai7_2Sau {
         String str2 = new Scanner(System.in).nextLine();
         //Phương thức contains() tìm kiếm chuỗi ký tự trong chuỗi này
         if(str1.contains(str2)){
-            int lenFirs = str1.indexOf(str2);
+            int lenFirst = str1.indexOf(str2);
             int len2 = str2.length();
-            int lenEnd = lenFirs + len2;
-            str11.delete(lenFirs, lenEnd);
+            int lenEnd = lenFirst + len2;
+            str11.delete(lenFirst, lenEnd);
         }
         System.out.println(str11);
     }
